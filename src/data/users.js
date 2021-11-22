98361e4a9eafc7bb7e9298ad2db1d35021abbb5b
Halil İbrahim KURT <halilibrahimkurtt10@gmail.com>
2021-11-22 22:56:02 +0300

    22.11.2021

---
+++ src/data/users.js
@@ -0,0 +1,45 @@
+export const users = [
+    {
+        id: 1,
+        firstName: "Engin",
+        lastName: "Demiroğ",
+        age: 36,
+        creditCartNumber: "123456",
+        type: "customer"
+    },
+    {
+        id: 2,
+        firstName: "Halil",
+        lastName: "Kurt",
+        city: "Balıkesir",
+        age: 25,
+        creditCartNumber: "654321",
+        type: "customer"
+    },
+    {
+        id: 3,
+        firstName: "Sercan",
+        lastName: "Atak",
+        city: "Manisa",
+        salary: 8000,
+        type: "employee"
+    },
+    {
+        id: 4,
+        firstName: "Onur",
+        lastName: "Kostak",
+        city: "Balıkesir",
+        age: 32,
+        type: "employee"
+    },
+    {
+        id: 5,
+        firstName: "Eren",
+        lastName: "Arı",
+        city: "Diyarbakır",
+        age: 20,
+        salary: 15000,
+        type: "employe"
+    }
+
+]
